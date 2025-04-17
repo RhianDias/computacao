22795f005c6833f7fa8094665d97575318c1c1fa
RhianDias <148086195+RhianDias@users.noreply.github.com>
2025-04-17 17:57:05 -0300

    Repeticao2.c

--- Repeticao2.c
+++ Repeticao2.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
-int main()
 
-{
-    int i=0;
-    float num, soma = 0.0;
+int main() {
+    int i = 0;
+    float num = 0.0, soma = 0.0;
 
-    printf("Digite um numero positivo(Negativos nao irao ser somados):\n(-1000 para finalizar)\n");
-    scanf("%f",&num);
+    printf("Digite um numero positivo (negativos nao irao ser somados):\n(-1000 para finalizar)\n");
+    scanf("%f", &num);
 
-    while (num != -1000)
-    {
-      if (num >= 0)
-        soma += num;
-        i++;
+    while (num != -1000) {
+        if (num >= 0) {
+            soma += num;
+            i++;
+        } else {
+            printf("Numeros negativos nao serao somados, digite outro numero:\n");
+        }
+        printf("Digite um numero positivo (negativos nao irao ser somados):\n(-1000 para finalizar)\n");
         scanf("%f", &num);
     }
 
-    float media = soma / i;
-    printf("A média é :%.1f\n", media);
+    if (i > 0) {
+        float media = soma / i;
+        printf("A media e: %.1f\n", media);
+    } else {
+        printf("Nenhum numero positivo foi digitado.\n");
+    }
 
-
-
-return 0;
+    return 0;
 }
